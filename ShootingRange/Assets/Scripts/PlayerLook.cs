4e50b76c827a2b9d4c9aa0bf508268d3c2aa6925
Dziemo <michalek210@gmail.com>
2020-12-14 13:02:35 +0100

    Basic movement

---
+++ ShootingRange/Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerLook : MonoBehaviour
+{
+    public float sensitivity = 2f;
+    public Vector2 maxAngle;
+    float xRot = 0;
+
+    Camera cam;
+    Vector2 rotVal;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        cam = Camera.main;
+    }
+
+    void OnLook (InputValue value)
+    {
+        rotVal = value.Get<Vector2>();
+    }
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.up, rotVal.x * sensitivity * Time.deltaTime);
+
+        xRot -= rotVal.y * sensitivity * Time.deltaTime;
+        xRot = Mathf.Clamp(xRot, maxAngle.x, maxAngle.y);
+
+        cam.transform.localRotation = Quaternion.Euler(xRot, 0, 0);
+    }
+}
---
+++ ShootingRange/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 5f;
+    
+    CharacterController chController;
+    Vector3 rawMoveDir;
+
+    void Start()
+    {
+        chController = GetComponent<CharacterController>();
+    }
+    
+    void OnMove (InputValue value)
+    {
+        rawMoveDir = value.Get<Vector2>();
+    }
+
+    private void Update()
+    {
+        var moveDir = transform.forward * rawMoveDir.y + transform.right * rawMoveDir.x;
+        chController.Move(Vector3.ClampMagnitude(moveDir, 1f) * movementSpeed * Time.deltaTime);
+    }
+}
